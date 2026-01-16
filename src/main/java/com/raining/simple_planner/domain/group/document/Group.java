9e10ceae4ac3f4412c67e17d1e2e3b504d8be3e9
Identityless <rlandwnd555@naver.com>
2026-01-16 11:51:22 +0900

    [FIX] 그룹 유저 아이디 및 플랜 아이디 필드 제네릭 타입 String으로 수정

--- src/main/java/com/raining/simple_planner/domain/group/document/Group.java
+++ src/main/java/com/raining/simple_planner/domain/group/document/Group.java
@@ -23,9 +23,9 @@ public class Group extends BaseDocument {
     private String name;                            // 그룹 이름(GroovyRoom)
     private String ownerId;                         // 그룹 생성자(소유자) ID
     private String description;                     // 그룹 설명
-    private List<Integer> planIds;                  // 그룹에 속한 플랜 ID 목록
+    private List<String> planIds;                   // 그룹에 속한 플랜 ID 목록
     private List<String> memberIds;                 // 그룹 멤버 ID 목록
-    private List<Integer> whiteBoardContentIds;     // 화이트보드 콘텐츠 ID 목록
+    private List<String> whiteBoardContentIds;      // 화이트보드 콘텐츠 ID 목록
 
     public GroupInfoResponseDTO parseResponseDTODetail() {
         return GroupInfoResponseDTO.builder()
